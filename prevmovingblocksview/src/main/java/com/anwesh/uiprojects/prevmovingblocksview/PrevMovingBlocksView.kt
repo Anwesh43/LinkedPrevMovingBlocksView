d3f9048b1b238792c90eb5ab930c5cc384eb6c13
Anwesh43 <anweshthecool0@gmail.com>
2019-04-09 10:34:44 +0530

    adding constants and libraries

--- prevmovingblocksview/src/main/java/com/anwesh/uiprojects/prevmovingblocksview/PrevMovingBlocksView.kt
+++ prevmovingblocksview/src/main/java/com/anwesh/uiprojects/prevmovingblocksview/PrevMovingBlocksView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.prevmovingblocksview
 /**
  * Created by anweshmishra on 09/04/19.
  */
+
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val squares : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 180f
+val parts : Int = 2
