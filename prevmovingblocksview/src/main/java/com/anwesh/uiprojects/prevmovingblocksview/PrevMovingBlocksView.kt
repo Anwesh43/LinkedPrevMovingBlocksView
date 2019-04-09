6df1d57221d3bc38e87ef09a60e79042afca4f48
Anwesh43 <anweshthecool0@gmail.com>
2019-04-09 10:38:10 +0530

    adding Int and Float extension methods

--- prevmovingblocksview/src/main/java/com/anwesh/uiprojects/prevmovingblocksview/PrevMovingBlocksView.kt
+++ prevmovingblocksview/src/main/java/com/anwesh/uiprojects/prevmovingblocksview/PrevMovingBlocksView.kt
@@ -24,3 +24,13 @@ val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 180f
 val parts : Int = 2
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorScale(a : Int, b : Int) : Float  {
+    val k : Float = scaleFactor()
+    return (1 - k ) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorScale(a, b) * dir * scGap
